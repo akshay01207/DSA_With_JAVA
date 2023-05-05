d21313d48a42974000608ea44e09edaa8c1fb19e
Akshay Kumar <110219277+akshay01207@users.noreply.github.com>
2023-05-05 23:10:26 +0530

    Add files via upload

---
+++ assignmentQus/BitwiseOptional5.java
@@ -0,0 +1,16 @@
+package com.assignmentQus;
+
+public class BitwiseOptional5 {
+	public static void main (String[] args) {
+		 	int a=10;
+		    a+=++a-5/3+6*a;
+		    System.out.print(a);
+/*
+a+=++a-5/3+6*a.
+a+=11-1+6*11.
+a=10+11-1+66
+=86.
+	     */
+	} 
+}
+

a7113d21cd72f377096ff941777f654a7da4aceb
Akshay Kumar <110219277+akshay01207@users.noreply.github.com>
2023-05-09 12:55:11 +0530

    SumOptional.java

--- functions/SumOptional.java
+++ functions/SumOptional.java
@@ -1,5 +1,20 @@
 package com.functions;
 
 public class SumOptional {
+	public static int sum(int a, int b) {
+		System.out.print("int sum ");
+		return a + b;
+	}
 
+	public static double sum(double a, double b) {
+		System.out.print("float sum ");
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		System.out.print(sum(5, 4));
+		System.out.print(sum(5.0, 4.0));
+	}
+// sum(5,4) would call the 1st Function and sum (5.0,4.0) would call 2nd Function
+  //int sum 9float sum 9.0
 }

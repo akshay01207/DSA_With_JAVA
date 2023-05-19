c4e112d169429743e9158f61e88492c5fdfc5fc9
Akshay Kumar <110219277+akshay01207@users.noreply.github.com>
2023-05-19 22:26:46 +0530

    Update PrintAllSubstring.java

--- strings/PrintAllSubstring.java
+++ strings/PrintAllSubstring.java
@@ -46,6 +46,13 @@ public class PrintAllSubstring {
 				System.out.println();
 			}
 		}
+		/*
+		for(int start=0; start<str.length();start++) {
+			for(int end=start;end<str.length();end++) {
+				System.out.println(str.substring(start, end+1));
+			}
+		}
+		*/
 	}
 
 	public static void main(String[] args) {

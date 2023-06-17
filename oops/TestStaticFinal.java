ebf7b2d80adf8d6fa60287f84ae1f76b64abffb4
Akshay Kumar <110219277+akshay01207@users.noreply.github.com>
2023-06-17 16:09:50 +0530

    Update TestStaticFinal.java

--- oops/TestStaticFinal.java
+++ oops/TestStaticFinal.java
@@ -11,8 +11,10 @@ class MCQ {
     public static void main (String[] args) {
     	TestStaticFinal t=new TestStaticFinal();              
         t.set_marks(78);                //Line 2
-        System.out.print(TestStaticFinal.marks);   //Line 3
+        System.out.print(TestStaticFinal.marks);   //Line 3 ==> 78
     }
 }
 
-
+//there is no error in this code. In this class, there is just a single variable, 
+//which is, static variable. Static variables are property of the class, but you can access it through objects. 
+//Hence, line 1 and 2 did not generate any error.  

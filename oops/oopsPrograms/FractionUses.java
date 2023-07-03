b1f8abdf1fff95b365497c5caf3c4cf8c0236734
Akshay Kumar <110219277+akshay01207@users.noreply.github.com>
2023-07-03 08:49:11 +0530

    Add files via upload

---
+++ oops/oopsPrograms/FractionUses.java
@@ -0,0 +1,17 @@
+package com.oopsPrograms;
+
+public class FractionUses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fraction f1= new Fraction(4,6);
+//		f1.print();
+//		f1.increment();
+		f1.print();
+
+		Fraction f2= new Fraction(4,8);
+		f1.add(f2);
+		f1.print();
+	}
+
+}
